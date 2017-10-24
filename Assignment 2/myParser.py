2588a2625ee36a2d215fbecacca690680d27556c
Dylan H. Parsons <dylan.parsons1@gmail.com>
2017-10-24 10:13:31 -0400

    merging

--- Assignment 2/myParser.py
+++ Assignment 2/myParser.py
@@ -18,6 +18,7 @@ import sys
 
 #Use global vars because it's well past midnight, otherwise it's a crime
 iNextToken = 0 #Next token index
+stack = []
 tokenStream = []
 lexemeStream = [] #not used for parsing
 stmtStart = ["print", "if", "while", "return", "id"]
@@ -59,24 +60,31 @@ def main(inputFileName):
         print("Valid Expression!")
 
 def program():
-    global iNextToken
+    global iNextToken, stack
+    stack.append("program")
     if tokenStream[:5] == ["type", "main", "(", ")", "{"]: # program must start with these tokens, excepting imports or global var declarations
         iNextToken += 5
         declarations()
         statements()
     else:
         error()
+    stack.pop()
 
 def declarations():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("declarations")
     # check if next token is a type
     while iNextToken < len(tokenStream) and tokenStream[iNextToken] == "type":
         iNextToken += 1
         # iteratively consume what's being declared
         declaration()
+    stack.pop()
 
 def declaration():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("declaration")
     #  consume an id
     id()
     while iNextToken < len(tokenStream) and tokenStream[iNextToken] == ",":
@@ -84,81 +92,101 @@ def declaration():
         # consume any following id, so long as it's preceded by ","
         id()
     closeStatement() # must end in semicolon
+    stack.pop()
 
 # consume a semicolon
 def closeStatement():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("closeStmt")
     if iNextToken < len(tokenStream) and tokenStream[iNextToken] == ";":
         iNextToken += 1
+        stack.pop()
         return
     else:
         error("Expected semicolon.")
 
 def id():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("id")
 
     if iNextToken < len(tokenStream) and tokenStream[iNextToken] == "id":
         # consume an id
         iNextToken += 1
+        stack.pop()
         return
     else:
         error()
 
 def statements():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("statements")
     
     while iNextToken < len(tokenStream) and tokenStream[iNextToken] in stmtStart:
-        print iNextToken, len(tokenStream)
         statement()
 
+    stack.pop()
+
 def statement():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("statement")
 
     if iNextToken >= len(tokenStream):
         error()
 
     if tokenStream[iNextToken] == "id":
-        print "assnhi"
         iNextToken += 1
         assignment()
     elif tokenStream[iNextToken] == "print":
-        print "printhi"
         iNextToken += 1
         printStmt()
     elif tokenStream[iNextToken] == "if":
-        print "ifhi"
         iNextToken += 1
         ifStmt()
     elif tokenStream[iNextToken] == "while":
-        print "whilehi"
         iNextToken += 1
         whileStmt()
     elif tokenStream[iNextToken] == "return":
-        print "returnhi"
         iNextToken += 1
         returnStmt()
     else:
         error()
 
+    stack.pop()
+
 def assignment():
-    global iNextToken
+    global iNextToken, stack
+    # remaining is assignOp Expression
+    stack.append("assignment")
     if iNextToken < len(tokenStream) and tokenStream[iNextToken] == "assignOp":
         iNextToken += 1
         expr()
         closeStatement()
+        stack.pop()
+        return
     else:
         error()
 
 def printStmt():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("print")
     if iNextToken < len(tokenStream):
         expr()
         closeStatement()
+        stack.pop()
+        return
     else:
         error()
 
 def ifStmt():
-    global iNextToken
+    global iNextToken, stack
+    
+    # must include parentheses
+    stack.append("if")
     if iNextToken < len(tokenStream) and tokenStream[iNextToken] == "(":
         iNextToken += 1
     else:
@@ -178,56 +206,144 @@ def ifStmt():
         iNextToken += 1
         statement()
 
+    stack.pop()
+
 def whileStmt():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("while")
+    
+    if iNextToken < len(tokenStream) and tokenStream[iNextToken] == "(":
+        iNextToken += 1
+    else:
+        error("Incomplete while statement.")
+    
+    expr()
+
+    if iNextToken < len(tokenStream) and tokenStream[iNextToken] == ")":
+        iNextToken += 1
+    else:
+        error("Incomplete while statement")
+
+    statement()
+
+    stack.pop()
 
 def returnStmt():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("return")
     if iNextToken < len(tokenStream):
         expr()
         closeStatement()
     else:
         error()
 
+    stack.pop()
+
 def expr():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("expr")
+    # consume single conjunction
+    conjunction()
+    while iNextToken < len(tokenStream) and \
+        (tokenStream[iNextToken] == "||"):
+        iNextToken += 1 # consume ||
+        conjunction() # consume another conjunction
+    stack.pop()
+
+def conjunction():
+    global iNextToken, stack
+
+    stack.append("conjunction")
+    equality()
+    # as many equalities as separated by &&
+    while iNextToken < len(tokenStream) and tokenStream[iNextToken] == "&&":
+        iNextToken += 1
+        equality()
+    stack.pop()
+
+def equality():
+    global iNextToken, stack
+
+    stack.append("equality")
+    relation()
+
+    if iNextToken < len(tokenStream) and tokenStream[iNextToken] == "equOp":
+        iNextToken += 1
+        relation()
+    stack.pop()
+
+def relation():
+    global iNextToken, stack
+    stack.append("relation")
+    addition()
+
+    if iNextToken < len(tokenStream) and tokenStream[iNextToken] == "relOp":
+        iNextToken += 1
+        addition()
+    stack.pop()
+
+def addition():
+    global iNextToken, stack
+    
+    stack.append("addition")
     
     term() #Consume a Term first
     while iNextToken < len(tokenStream) and \
-        (tokenStream[iNextToken] == "+" or tokenStream[iNextToken] == "-"):
+        (tokenStream[iNextToken] == "addOp"):
         iNextToken += 1 #Consumed the + or - token
         term() #Consume another Term
+    stack.pop()
 
 def term():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("term")
     
     factor() #Consume a Factor
     while iNextToken < len(tokenStream) and \
-        (tokenStream[iNextToken] == "*" or tokenStream[iNextToken] == "/"):
+        (tokenStream[iNextToken] == "multOp"):
         iNextToken += 1 #Consumed the * or / token
         factor() #Consume another Factor
+    stack.pop()
 
 def factor():
-    global iNextToken
+    global iNextToken, stack
+    
+    stack.append("factor")
     
     if iNextToken < len(tokenStream):
+        # check if any literal (char not implemented)
         if tokenStream[iNextToken] in literals:
             iNextToken += 1
+            stack.pop()
             return
+        elif tokenStream[iNextToken] == "(":
+            # beginning of (Expression)
+            iNextToken += 1
+            expr()
+            if iNextToken < len(tokenStream) and tokenStream[iNextToken] == ")":
+                iNextToken += 1
+            else:
+                error("Expected closing parenthesis.")
         else:
             id()
-
     else:
         error()
+    stack.pop()
 
 def error(message = ""):
-    #print(iNextToken)
     if message != "":
-        print("Error: " + message + "Error location: < " + tokenStream[iNextToken] + ", " + lexemeStream[iNextToken] + " >.")
+        print("Error: " + message + " Error location: < " + tokenStream[iNextToken] + ", " + lexemeStream[iNextToken] + " >.")
     elif iNextToken < len(tokenStream):
-        print("Error: Invalid expression. Error location: <" + tokenStream[iNextToken] + "," + lexemeStream[iNextToken] + ">.")
+        print("Error: Invalid expression. Error location: < " + tokenStream[iNextToken] + "," + lexemeStream[iNextToken] + " >.")
     else:
         print("Error: Incomplete expression. Expecting more terms.")
+    print "\nStack is:"
+    for i in stack:
+        print i + "->",
     exit()
 
 #To take command line argument for the file name
